cd55fff72801c6b0b8baa0b29ea988bb32e7c315
lucile-sticky <lucile@stickyads.tv>
2015-12-03 11:42:36 +0100

    Correction de la faute Otello -> Othello

--- Othello/src/othello/Othello.java
+++ Othello/src/othello/Othello.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package otello;
+package othello;
 
 /**
  *
  * @author lucile
  */
-public class Otello {
+public class Othello {
 
     /**
      * @param args the command line arguments
